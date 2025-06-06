7e787be4fc317567866e47db5776e522824fe1f7
LeeWonHee <lwh4022>
2025-06-06 15:19:56 +0900

    setting

---
+++ src/main/kotlin/com/hangxomviet/kotlinbe/KotlinBeApplication.kt
@@ -0,0 +1,11 @@
+package com.hangxomviet.kotlinbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/hangxomviet/kotlinbe/KotlinBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hangxomviet.kotlinbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
